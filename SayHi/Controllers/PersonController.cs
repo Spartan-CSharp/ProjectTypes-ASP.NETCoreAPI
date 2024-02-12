774dff253a2d9165a610c12287869f9e3d150684
Pierre Plourde <pierre@spartancsharp.net>
2024-02-12 16:47:53 -0500

    Create Code
    Build a Web API Application that has a GET call that takes in a First Name and Last Name and it returns "Hi {FN} {LN}".

---
+++ SayHi/Controllers/PersonController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SayHi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class PersonController : ControllerBase
+	{
+		// GET: api/Person
+		[HttpGet]
+		public string Get(string firstName, string lastName)
+		{
+			return $"Hi {firstName} {lastName}!";
+		}
+	}
+}
---
+++ SayHi/Models/PersonModel.cs
@@ -0,0 +1,20 @@
+﻿namespace SayHi.Models
+{
+	public class PersonModel
+	{
+		public int Id
+		{
+			get; set;
+		}
+
+		public string FirstName
+		{
+			get; set;
+		}
+
+		public string LastName
+		{
+			get; set;
+		}
+	}
+}
